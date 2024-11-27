4450cc8e0578f35b5f9e8fb1a7bdc11c4261c632
Shazna1 <139617357+Shazna1@users.noreply.github.com>
2024-11-27 17:30:12 +0000

    replace

---
+++ assignment4/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AnimalTable from './component/AnimalTable';
+
+function App() {
+  return (
+    <div className="App">
+      <AnimalTable/>
+    </div>
+  );
+}
+
+export default App;
